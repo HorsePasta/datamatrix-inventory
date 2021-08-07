ceaae2b693d07317516a4a3ab86aaa8a104c910d
MahxieNoodle <mahx@mahxdesigns.com>
2021-08-07 11:38:01 -0600

    Move duplicate html into the table include file for ease of upkeep

--- src/index.php
+++ src/index.php
@@ -21,40 +21,7 @@
 </div>
 
 <!-- CSV Container -->
-<div class="grid-x fluid">
-    <div class="cell">
-    <div class="csv-display" id="csv-display">
-        <!-- CSV Table -->
-        <h4 class="text-center">Scanned Assets</h4>
-        <div class="table-scroll">
-        <table id="csv-display-table">
-            <tbody>
-            <tr>
-                <th>Name</th>
-                <th>Manufacturer</th>
-                <th>Model</th>
-                <th>Asset Tag</th>
-                <th>Serial</th>
-                <th>Product #</th>
-                <th>MAC</th>
-                <th>Room#</th>
-                <th>Action</th>
-                <th>To Room#</th>
-                <th>Notes</th>
-                <th></th>
-            </tr>
-            </tbody>
-
-            <tbody id="csv-display-table-data"></tbody>
-
-        </table>
-        </div>
-
-        <button type="button" id="csv-reset" class="button  alert float-left" onclick="manageCsv.clear()">Clear CSV</button>
-        <button type="button" id="csv-save" class="button  primary float-right" onclick="manageCsv.save()">Save CSV</button>
-    </div>
-    </div>
-</div>
+<?php require_once 'table.php'; ?>
 
 <!-- Datamatrix Display -->
 <div class="grid-x fluid">
